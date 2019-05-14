cc1ff134363a99734d7387724a968639676bb0db
rebfarish <rebfarish@gmail.com>
2019-05-14 11:15:54 -0600

    initial commit

--- IntsToWords/src/edu/cnm/deepdive/Converter.java
+++ IntsToWords/src/edu/cnm/deepdive/Converter.java
@@ -2,4 +2,87 @@ package edu.cnm.deepdive;
 
 public class Converter {
 
-}
+    /**
+     * Converts any integer value to words (according to the conventions followed
+     * for checks and other instruments) and returns the resulting string.
+     */
+
+    public static void main(String[] args) {
+      for (int i = 0; i<=100; i++){//i can be any integer
+        System.out.println(formatAsWords(i));
+      }
+
+    }
+
+    public static String formatAsWords(int value) {
+
+      if (value < 0 ){
+        throw new IllegalArgumentException();
+      }
+      if(value < 20){
+        return units[value];
+      }
+      if (value < 100){
+        return tens[value/10] + units[value % 10];
+      }
+      if (value < 1000){
+        return (units[value/100] + " hundred " + formatAsWords(value % 100)).trim();
+
+      }
+      if (value < 1000000){
+        return (formatAsWords(value/1000) + " thousand " + formatAsWords(value % 1000)).trim();
+      }
+
+      if (value < 1000000000){
+        return (formatAsWords(value/1000000) + " million " + formatAsWords(value % 1000000)).trim();
+      }
+
+      else{
+        return formatAsWords(value/1000000000) + " billion " + formatAsWords(value % 1000000000);
+
+      }
+    }
+
+    public  static final String[] units = {
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen"
+    };
+
+    public static final String[] tens = {
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety"
+    };
+
+
+  }
+
+
+
+
+
--- PrimeNumbers/src/biz/enenbe/Primes.java
+++ PrimeNumbers/src/biz/enenbe/Primes.java
@@ -2,4 +2,30 @@ package biz.enenbe;
 
 public class Primes {
 
+  public static void main(String[] args) {
+    System.out.println(isPrime(3627));
+  }
+
+  public static boolean isPrime(int value) {
+
+    boolean isPrime = true;
+    if ((value % 10 == 0) || (value % 10 == 5) || (value % 10 % 2 == 0)) { //if last digit is 0 or 5 or divisible by 2
+
+      isPrime = false;
+//    } else if (value>100) {
+//      if (((value % 10) + ((value / 10) % 10) + ((value / 100) % 10)) % 9 == 0) {
+//        isPrime = false;
+//      }
+    } else {
+      for (int i = 2; i < value; i++) {
+        if (value % i == 0) {
+          isPrime = false;
+        }
+      }
+    }
+    return isPrime;
+  }
+
 }
+
+
